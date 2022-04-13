70a4be2acfba362488e3edccaca50fc76486451b
Spencer Haugh <spencer.haugh@documo.com>
2022-04-12 22:15:34 -0400

    initialize app

---
+++ src/components/HangryApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function HangryApp() {
+    return (
+        <div class="hangry-app">
+            <h1>Hangry</h1>
+            <h3>When you need meal ideas, now.</h3>
+            <p>[logo here]</p>
+            {/* Meal Generator component */}
+            {/* Results display component */}
+        </div>
+    )
+}
